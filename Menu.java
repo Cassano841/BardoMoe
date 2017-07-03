0b6ccad4703938e100f4290513e322ea6713cc6c
Cassano <ninocb@gmail.com>
2017-07-03 19:54:32 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,6 +2,7 @@ package br.com.ifrsrestinga.progii.entidades;
 import java.util.Scanner;
 
 import br.com.ifrsrestinga.progii.testes.TestaClienteDAO;
+import br.com.ifrsrestinga.progii.testes.TestaOperacoes;
 import br.com.ifrsrestinga.progii.testes.TestaProdutoDAO;
 
 public class Menu {
@@ -10,29 +11,29 @@ public class Menu {
 		Scanner n = new Scanner(System.in);
 		int op = 0;
 		System.out.println("Escolha a função que deseja realizar:");
-		System.out.println("--------------------------");
-		System.out.println("|1 - Clientes             |");
-		System.out.println("|2 - Produtos             |");
-		System.out.println("|3 - Relatorios           |");
-		System.out.println("|4 - Vendas               |");
-		System.out.println("|4 - Sair                 |");
-		System.out.println("---------------------------");
+		System.out.println("-----------------");
+		System.out.println("|1 - Clientes    |");
+		System.out.println("|2 - Produtos    |");
+		System.out.println("|3 - Relatorios  |");
+		System.out.println("|4 - Vendas      |");
+		System.out.println("------------------");
 		op = n.nextInt();
 		
 		switch (op) {
 		case 1:
-			System.out.println("Chama TestaCLiente"); // será que é possivel?
+			System.out.println("Chama CLiente");
 			TestaClienteDAO cliente  = new TestaClienteDAO();
 			break;
 		case 2:
-			System.out.println("Chama TestaProduto"); // será que é possivel?
+			System.out.println("Chama Produto");
 			TestaProdutoDAO produto = new TestaProdutoDAO();
 			break;
 		case 3:
 		//	acessoRelatorios();
 			break;
 		case 4:
-		//	sair();
+			System.out.println("Chama Operações");
+			TestaOperacoes operacao = new TestaOperacoes();
 			break;
 		default:
 			System.out.println("Opcao invalida!");
